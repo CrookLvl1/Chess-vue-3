2b388af9c73261b6f7d8fc3067015dcd9ee7dce6
Konstantin <kostzis693@gmail.com>
2023-09-03 16:07:10 +0500

    welpyeah

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,13 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: '/',
             name: 'home',
+            
             component: () => import('@/views/HomeView.vue')
         },
         // route level code-splitting
