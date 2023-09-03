de6c673702681e18a1da495697cdaf3cd1b4b3a6
Konstantin <kostzis693@gmail.com>
2023-09-03 15:51:49 +0500

    probably final commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/first-vite-project',
+  base: '/Chess-vue-3',
   plugins: [
     vue()
   ],
