aff52e45d7572fec153900a42f7a7871f98754cc
Konstantin <kostzis693@gmail.com>
2023-09-03 13:51:21 +0500

    gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/chess-vue-3/',
-  server: {
-    host: true,
-    port: 5173
-  },
   plugins: [
     vue()
     
